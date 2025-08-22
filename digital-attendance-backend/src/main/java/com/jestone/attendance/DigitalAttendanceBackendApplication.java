dea58394d8121799e46f9f45061f9f9b141dfb54
Jestone <jestone002@gmail.com>
2025-08-22 15:15:01 +0300

    project setup, backend

---
+++ digital-attendance-backend/src/main/java/com/jestone/attendance/DigitalAttendanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jestone.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAttendanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAttendanceBackendApplication.class, args);
+	}
+
+}
---
+++ digital-attendance-backend/src/test/java/com/jestone/attendance/DigitalAttendanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jestone.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAttendanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
